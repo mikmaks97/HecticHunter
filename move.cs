5784df6fe404a3a8151c81e48fa14f0b1ff232a8
Maximilian Ioffe <maxioffe@gmail.com>
2015-11-24 16:56:15 -0500

    Movement and popUp scripts
    Player movement script (can be removed) + script for pop up messages

---
+++ move.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public float speed = 5f;
+
+	Vector3 mousePosOnlyX;
+	Vector3 mousePos;
+	private float startTime;
+	private float journeyLength;
+	private bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) {
+			mousePos = Input.mousePosition;
+			mousePos.z = 10.0f;
+			mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+			mousePosOnlyX = new Vector3(mousePos.x, transform.position.y, mousePos.z);
+
+			startTime = Time.time;
+			journeyLength = Vector3.Distance(transform.position, mousePosOnlyX);
+
+			moving = true;
+		}
+
+		if (moving) {
+			float distCovered = (Time.time - startTime) * speed;
+			float fracJourney = distCovered / journeyLength;
+			transform.position = Vector3.Lerp (transform.position, mousePosOnlyX, fracJourney); //speed*Time.deltaTime
+		}
+	}
+}
---
+++ popUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class popUp : MonoBehaviour {
+
+	public TextMesh textMesh;
+	string[] textAndObject;
+	public move playerMoveScript;
+	
+	// Use this for initialization
+	public void popUpClick(string message) {
+		textAndObject = message.Split ('|');
+		textMesh.text = textAndObject[0];
+		textMesh.gameObject.transform.position = GameObject.Find (textAndObject[1]).transform.position;
+	}
+}
