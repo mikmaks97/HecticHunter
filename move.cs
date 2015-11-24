f3f3dcc6d7f96ecf8768eb45a6b3355d0497c140
Maximilian Ioffe <maxioffe@gmail.com>
2015-11-24 17:35:46 -0500

    Scripts updated with comments
    Added comments to the popUp and move script, so check em’ out!

--- move.cs
+++ move.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class move : MonoBehaviour {
 
+	//player's moving speed
 	public float speed = 5f;
 
-	Vector3 mousePosOnlyX;
+	//mouse position variables
+	Vector3 mousePosOnlyX; //we need it to move only horizontally
 	Vector3 mousePos;
+
 	private float startTime;
 	private float journeyLength;
 	private bool moving = false;
@@ -18,22 +21,28 @@ public class move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//if the left mouse button is pressed...
 		if (Input.GetMouseButtonUp (0)) {
-			mousePos = Input.mousePosition;
-			mousePos.z = 10.0f;
-			mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+			mousePos = Input.mousePosition; //get the mouse position
+			mousePos.z = 10.0f; //set the z-distance
+			mousePos = Camera.main.ScreenToWorldPoint(mousePos); //convert pixels to Unity world units
 
+			//take only the x and z positions from the mouse
 			mousePosOnlyX = new Vector3(mousePos.x, transform.position.y, mousePos.z);
 
+			//this is for smooth movement
 			startTime = Time.time;
 			journeyLength = Vector3.Distance(transform.position, mousePosOnlyX);
 
+			//we're moving!
 			moving = true;
 		}
 
+		//if the player is moving...
 		if (moving) {
 			float distCovered = (Time.time - startTime) * speed;
 			float fracJourney = distCovered / journeyLength;
+			//Lerp means to smoothly transition. so we are smoothly transitioning from the player's current position to the mouse position in 'fracJourney' amount of time
 			transform.position = Vector3.Lerp (transform.position, mousePosOnlyX, fracJourney); //speed*Time.deltaTime
 		}
 	}
--- popUp.cs
+++ popUp.cs
@@ -2,15 +2,17 @@
 using System.Collections;
 
 public class popUp : MonoBehaviour {
-
+	
 	public TextMesh textMesh;
 	string[] textAndObject;
 	public move playerMoveScript;
-	
-	// Use this for initialization
+
+	//when an object is clicked that needs a popup...
 	public void popUpClick(string message) {
-		textAndObject = message.Split ('|');
-		textMesh.text = textAndObject[0];
+		//we split the message up into two things: the actual text and the name of the object that was pressed
+		textAndObject = message.Split ('|'); //we split using this character '|'
+		textMesh.text = textAndObject[0]; //the text goes into the text mesh
+		//the text mesh's position is equal to the position of the object that we find by its name :)
 		textMesh.gameObject.transform.position = GameObject.Find (textAndObject[1]).transform.position;
 	}
 }
