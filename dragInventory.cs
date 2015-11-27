f946deeb8557405435587ac8c457e0b784659fe6
Maximilian Ioffe <maxioffe@gmail.com>
2015-11-27 12:33:35 -0500

    Inventory scripts
    Created an inventory script, which keeps track of the inventory and a
    script that gets triggered, when you try to drag objects from the
    inventory. All scripts have been tested and work :)

---
+++ dragInventory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class dragInventory : MonoBehaviour {
+
+	Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position; //stores the start position of the inventory to then return back to it
+	}
+
+	//triggered when we drag an inventory object
+	public void mouseDrag() {
+		transform.position = Input.mousePosition;
+	}
+
+	//triggered when we stop dragging
+	public void backToOriginal() {
+		transform.position = startPosition;
+	}
+}
--- popUp.cs
+++ popUp.cs
@@ -1,18 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class popUp : MonoBehaviour {
 	
 	public TextMesh textMesh;
+	public Transform popUpLocation;
+	public inventory inventoryScript;
+
 	string[] textAndObject;
-	public move playerMoveScript;
 
 	//when an object is clicked that needs a popup...
 	public void popUpClick(string message) {
 		//we split the message up into two things: the actual text and the name of the object that was pressed
 		textAndObject = message.Split ('|'); //we split using this character '|'
-		textMesh.text = textAndObject[0]; //the text goes into the text mesh
-		//the text mesh's position is equal to the position of the object that we find by its name :)
-		textMesh.gameObject.transform.position = GameObject.Find (textAndObject[1]).transform.position;
+		textMesh.text = textAndObject[0];
+		//the text mesh's position is equal to the position of the player :)
+		textMesh.gameObject.transform.position = popUpLocation.transform.position;
+
+		//store the sprite of the clicked-on object
+		Sprite inventoryObject = GameObject.Find (textAndObject [1]).GetComponent<SpriteRenderer>().sprite;
+		if (inventoryObject != null) { //if the sprite isn't null, i.e. we clicked on an object that should go in the inventory
+			inventoryScript.inventoryObjects.Add (inventoryObject); //add it to the list of inventory objects
+			inventory.currentInventoryIndex = inventoryScript.inventoryObjects.Count-1; //display it immediately
+			GameObject.Find (textAndObject [1]).SetActive(false); //hide the actual object from the world
+		}
 	}
 }
